75d1b357de77133853f96cefaead37647797d5ea
Nguyen Duy Minh Thong <nguyenduyminhthong@gmail.com>
2024-01-29 18:51:27 +0700

    Refactor logging in training_api.py to include timestamp

--- src/api/training_api.py
+++ src/api/training_api.py
@@ -15,12 +15,12 @@ class TrainingModelRequest(BaseModel):
     question: str | None = None
     ddl: str | None = None
     doc: str | None = None
-    time_created: str = get_formatted_time()
 
 
 @router.post("/train_model")
 def train_model(request: TrainingModelRequest) -> Response:
-    logger.info(f"Training model with request: {request}")
+    time_created = get_formatted_time()
+    logger.info(f"Training model with request: {request} at {time_created}")
 
     try:
         db_agent = ChromaDBAgent(request.vector_db_host)
@@ -31,15 +31,15 @@ def train_model(request: TrainingModelRequest) -> Response:
                 raise ValueError("Question is not provided.")
 
             logger.info(f"Adding question: {request.question}")
-            db_agent.add_sql_question(request.sql, request.question, request.time_created)
+            db_agent.add_sql_question(request.sql, request.question, time_created)
 
         if request.doc:
             logger.info(f"Adding doc: {request.doc}")
-            db_agent.add_doc(request.doc, request.time_created)
+            db_agent.add_doc(request.doc, time_created)
 
         if request.ddl:
             logger.info(f"Adding ddl: {request.ddl}")
-            db_agent.add_ddl(request.ddl, request.time_created)
+            db_agent.add_ddl(request.ddl, time_created)
 
         return Response(status_code=status.HTTP_200_OK)
 
